bfdeed33b2c7d6ee323adee6b4e32ab4dc5eb7f8
KarinaAstral <karina.kresnadi@astral.com.au>
2024-12-05 16:30:53 +1100

    code runs

--- compiler.js
+++ compiler.js
@@ -1,9 +1,10 @@
-module.exports = function compiler(input) {
+import tokeniser from './tokeniser.js';
+
+export default function compiler(input) {
     // 1. Lexical Analysis - Break the input code (string) into the basic syntax of language (array of objects)
     const tokens = tokeniser(input);
     // 2. Syntactic Analysis
     // 3. Transformation
     // 4. Code Generation
-    //
-    // return jsCode;
-  }
+    return tokens;
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const compiler = require('./compiler');
+import compiler from './compiler.js';
+
 const input = '(add 2 (sub 4 3))';
 const output = compiler(input);
-console.log(output);
+console.log(JSON.stringify(output, null, 2));
